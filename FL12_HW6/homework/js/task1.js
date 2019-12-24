8adb562652fb9520323f21263785574f8f3362ed
Alesya103 <alesya.afanaseva27@gmail.com>
2019-12-24 22:05:01 +0200

    changed js files according to task

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,38 +1,21 @@
-let flag = true;
-let a, b, c ='';
 let k4 = 4;
 let k2 = 2;
-while (flag) {
-  a = parseFloat(prompt('print the first (a) parametr of the quadratic equation'));
-  if( a < 0 || a > 0 ) {
-    flag = false;
+let a = parseFloat(prompt('print the first (a) parametr of the quadratic equation'));
+let b = parseFloat(prompt('print the second (b) parametr of the quadratic equation'));
+let c = parseFloat(prompt('print the third (c) parametr of the quadratic equation'));
+if ((a < 0 || a > 0) && (b <= 0 || b >= 0) && (c <= 0 || c >= 0)){
+  let D = b*b - k4*a*c;
+  if(D < 0) {
+    console.log('no solution');
+  } else if (D > 0) {
+    console.log('x1 =', Math.round((-b + Math.sqrt(D))/k2/a), 'and x2 =', Math.round((-b - Math.sqrt(D))/k2/a));
   } else {
-    alert('Invalid input data');
+    let x = Math.round(-b/k2/a);
+    if (x === 0) {
+      x = 0;
+    }
+    console.log('x =', x);
   }
-}
-flag = true;
-while (flag) {
-  b = parseFloat(prompt('print the second (b) parametr of the quadratic equation'));
-  if( b <= 0 || b >= 0 ) {
-    flag = false;
-  } else {
-    alert('Invalid input data');
-  }
-}
-flag = true;
-while (flag) {
-  c = parseFloat(prompt('print the third (c) parametr of the quadratic equation'));
-  if( c <= 0 || c >= 0 ) {
-    flag = false;
-  } else {
-    alert('Invalid input data');
-  }
-}
-let D = b*b - k4*a*c;
-if(D < 0) {
-  console.log('no solution');
-} else if (D > 0) {
-  console.log('x1 =',Math.round((-b + Math.sqrt(D))/k2/a), 'and x2 =',Math.round((-b - Math.sqrt(D))/k2/a));
 } else {
-  console.log('x =',Math.round(-b/k2/a));
+  console.log('Invalid input data');
 }
--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,37 +1,18 @@
-let flag = true;
-let a, b, c ='';
-while (flag) {
-  a = parseInt(prompt('print the first (a) side of triangle'));
-  if( a <= 0 || a >= 0) {
-    flag = false;
+let a = parseInt(prompt('print the first (a) parametr of the quadratic equation'));
+let b = parseInt(prompt('print the second (b) parametr of the quadratic equation'));
+let c = parseInt(prompt('print the third (c) parametr of the quadratic equation'));
+if ( a > 0 && b > 0 && c > 0) {
+  if ( a + b <= c || a + c <= b || c + b <= a ) {
+    alert('Triangle doesn’t exist');
+  } else if ( a === b && b === c) {
+    alert('Equilateral triangle');
+  } else if ( a === b || a === c || b === c ) {
+    alert('Isosceles triangle');
   } else {
-    alert('input values should be ONLY numbers');
+    alert('Scalene triangle');
   }
-}
-flag = true;
-while (flag) {
-  b = parseInt(prompt('print the second (b) side of triangle'));
-  if( b <= 0 || b >= 0) {
-    flag = false;
-  } else {
-    alert('input values should be ONLY numbers');
-  }
-}
-flag = true;
-while (flag) {
-  c = parseInt(prompt('print the third (c) side of triangle'));
-  if( c <= 0 || c >= 0) {
-    flag = false;
-  } else {
-    alert('input values should be ONLY numbers');
-  }
-}
-if ( a + b <= c || a + c <= b || c + b <= a ) {
-  alert('Triangle doesn’t exist');
-} else if ( a === b && b === c) {
-  alert('Equilateral triangle');
-} else if ( a === b || a === c || b === c ) {
-  alert('Isosceles triangle');
+} else if (a === 0 || b === 0 || c === 0) {
+  alert('A triangle must have 3 sides with a positive definite length');
 } else {
-  alert('Scalene triangle');
+  alert('input values should be ONLY numbers');
 }
