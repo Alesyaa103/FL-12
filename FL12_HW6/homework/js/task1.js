5a543ae577440440c59d3efcc1d515031df63181
Alesya103 <alesya.afanaseva27@gmail.com>
2019-12-24 21:05:09 +0200

    sixth homework done!

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,1 +1,38 @@
-// Your code goes here
+let flag = true;
+let a, b, c ='';
+let k4 = 4;
+let k2 = 2;
+while (flag) {
+  a = parseFloat(prompt('print the first (a) parametr of the quadratic equation'));
+  if( a < 0 || a > 0 ) {
+    flag = false;
+  } else {
+    alert('Invalid input data');
+  }
+}
+flag = true;
+while (flag) {
+  b = parseFloat(prompt('print the second (b) parametr of the quadratic equation'));
+  if( b <= 0 || b >= 0 ) {
+    flag = false;
+  } else {
+    alert('Invalid input data');
+  }
+}
+flag = true;
+while (flag) {
+  c = parseFloat(prompt('print the third (c) parametr of the quadratic equation'));
+  if( c <= 0 || c >= 0 ) {
+    flag = false;
+  } else {
+    alert('Invalid input data');
+  }
+}
+let D = b*b - k4*a*c;
+if(D < 0) {
+  console.log('no solution');
+} else if (D > 0) {
+  console.log('x1 =',Math.round((-b + Math.sqrt(D))/k2/a), 'and x2 =',Math.round((-b - Math.sqrt(D))/k2/a));
+} else {
+  console.log('x =',Math.round(-b/k2/a));
+}
--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,1 +1,37 @@
-// Your code goes here
+let flag = true;
+let a, b, c ='';
+while (flag) {
+  a = parseInt(prompt('print the first (a) side of triangle'));
+  if( a <= 0 || a >= 0) {
+    flag = false;
+  } else {
+    alert('input values should be ONLY numbers');
+  }
+}
+flag = true;
+while (flag) {
+  b = parseInt(prompt('print the second (b) side of triangle'));
+  if( b <= 0 || b >= 0) {
+    flag = false;
+  } else {
+    alert('input values should be ONLY numbers');
+  }
+}
+flag = true;
+while (flag) {
+  c = parseInt(prompt('print the third (c) side of triangle'));
+  if( c <= 0 || c >= 0) {
+    flag = false;
+  } else {
+    alert('input values should be ONLY numbers');
+  }
+}
+if ( a + b <= c || a + c <= b || c + b <= a ) {
+  alert('Triangle doesn’t exist');
+} else if ( a === b && b === c) {
+  alert('Equilateral triangle');
+} else if ( a === b || a === c || b === c ) {
+  alert('Isosceles triangle');
+} else {
+  alert('Scalene triangle');
+}
