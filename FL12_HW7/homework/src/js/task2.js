8867ed26f49c39322e72e07f440e34f99a2e3022
Alesya103 <alesya.afanaseva27@gmail.com>
2019-12-27 00:11:09 +0200

    change according to task

--- FL12_HW7/homework/src/js/task2.js
+++ FL12_HW7/homework/src/js/task2.js
@@ -5,20 +5,23 @@ if (!activate) {
   let next = true;
   let price = 0;
   let iteration = 0;
+  let maxNumber = 8;
+  let maxIter = 3;
+  let minWin = 25;
   while ( next ) {
-    let gessed = Math.floor(Math.random() * 8).toString();
+    let gessed = Math.floor(Math.random() * maxNumber).toString();
     console.log(gessed);
     let number = 10;
     let i = 0;
-    while (gessed !== number && i < 3) {
+    while (gessed !== number && i < maxIter) {
       i++;
       number = prompt( `Choose a roulette pocket number from 0 to 8
 Attempts left: ${4-i}
 Total prize: ${price}$
-Possible prize on current attempt: ${25 * Math.pow(2, 3-i ) * Math.pow(2,iteration)}$`);
+Possible prize on current attempt: ${minWin * Math.pow(2, maxIter-i ) * Math.pow(2,iteration)}$`);
     }
     if (gessed === number) {
-      price += 25 * Math.pow(2,3-i) * Math.pow(2,iteration)
+      price += minWin * Math.pow(2,maxIter-i) * Math.pow(2,iteration)
       next = confirm(`Congratulation, you won!   Your prize is: ${price}$. Do you want to continue?`);
       if (!next) {
         alert(`Thank you for your participation. Your prize is: ${price}$`);
