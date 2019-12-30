ed81954596c9523f84e3382ce9689d2f26b896f4
Alesya103 <alesya.afanaseva27@gmail.com>
2019-12-30 21:44:13 +0200

    change isLeapYear

--- FL12_HW8/homework/js/isLeapYear.js
+++ FL12_HW8/homework/js/isLeapYear.js
@@ -1,24 +1,14 @@
-function isLeapYear(data) {
-  if (typeof (data) === 'number') {
-    let date = new Date(data);
-    let month = date.getMonth();
-    let year = date.getFullYear();
-    let day = date.getDate();
-    if (!year) {
-      return ('Invalid Date')
-    } else if (new Date(year, month, day).getDate() === 29) {
-      return (`${year} is a leap year`);
-    } else {
-      return (`${year} is not a leap year`);
-    }
+function isLeapYear(inputDate) {
+  let date = new Date(inputDate);
+  if (date instanceof Date && !isNaN(date)) {
+      let year = date.getFullYear();
+      if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+        return (`${year} is a leap year`);
+      } else {
+        return (`${year} is not a leap year`);
+      }
   } else {
-    data = data.split('');
-    let year = data.slice(0,4).join('');
-    if ( (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
-      return (`${year} is a leap year`);
-    } else {
-      return (`${year} is not a leap year`);
-    }
+    return 'Invalid Date';
   }
 }
 isLeapYear(1213131313);
