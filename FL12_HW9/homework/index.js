208638bf1286457efe3739e4435448b2ac1667f9
Alesya103 <alesya.afanaseva27@gmail.com>
2020-01-14 00:41:47 +0200

    add missing functions; formate the code

--- FL12_HW9/homework/index.js
+++ FL12_HW9/homework/index.js
@@ -1,10 +1,11 @@
-function convert() {
+const convert = function () {
   let a = [];
   for (let i = 0; i < arguments.length; i++) {
     typeof arguments[i] === 'string' ? a[i] = +arguments[i] : a[i] = arguments[i].toString()
   }
   return a;
 }
+
 convert('1', 2, 3, '4');
 
 const executeforEach = function (arr, cb) {
@@ -13,22 +14,24 @@ const executeforEach = function (arr, cb) {
     cb(arg, i, arr);
   }
 }
+
 executeforEach([1, 2, 3], function (el) {
-  console.log(el * 2)
+  console.log(el * 2);
 });
 
-function mapArray(arr, cb) {
+const mapArray = function (arr, cb) {
   let newArr = [];
   executeforEach(arr, function (arg, i, arr) {
     newArr.push(cb(arg, i, arr));
   });
   return newArr;
 }
-mapArray([2, '5', 8], function (el) {
-  return parseInt(el) + 3
-})
 
-function filterArray(arr, cb) {
+mapArray([2, '5', 8], function (el) {
+  return parseInt(el) + 3;
+});
+
+const filterArray = function (arr, cb) {
   let newArr = [];
   executeforEach(arr, function (arg, i, arr) {
     if (cb(arg, i, arr)) {
@@ -39,10 +42,10 @@ function filterArray(arr, cb) {
 }
 
 filterArray([2, 5, 8], function (el) {
-  return el % 2 === 0
-})
+  return el % 2 === 0;
+});
 
-function flipOver(str) {
+const flipOver = function (str) {
   let newStr = '';
   for (let i = 0; i < str.length; i++) {
     newStr += str[str.length - i - 1];
@@ -52,7 +55,7 @@ function flipOver(str) {
 
 flipOver('hey world');
 
-function makeListFromRange(arr) {
+const makeListFromRange = function (arr) {
   let newArr = [];
   for (let i = 0; i <= arr[1] - arr[0]; i++) {
     newArr[i] = arr[0] + i;
@@ -60,7 +63,7 @@ function makeListFromRange(arr) {
   return newArr;
 }
 
-makeListFromRange([2, 7])
+makeListFromRange([2, 7]);
 
 const actors = [{
     name: 'tommy',
@@ -72,7 +75,7 @@ const actors = [{
   }
 ];
 
-function getArrayOfKeys(arr, key) {
+const getArrayOfKeys = function (arr, key) {
   let newArr = [];
   executeforEach(arr, function (arg) {
     newArr.push(arg[key]);
@@ -82,10 +85,27 @@ function getArrayOfKeys(arr, key) {
 
 getArrayOfKeys(actors, 'name');
 
-function substitute(arr) {
+const substitute = function (arr) {
   return mapArray(arr, function (arg) {
     return arg > 30 ? '*' : arg;
   })
 }
 
 substitute([58, 14, 48, 2, 31, 29]);
+
+const date = new Date(2019, 0, 2);
+
+const getPastDay = function (date, num) {
+  return new Date(+date - num * 3600 * 24 * 1000).getDate();
+}
+
+getPastDay(date, 1);
+getPastDay(date, 2);
+getPastDay(date, 365);
+
+const formatDate = function (date) {
+  return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()} ${date.getHours()}:${date.getMinutes()}`;
+}
+
+formatDate(new Date('6/15/2018 09:15:00'));
+formatDate(new Date());
